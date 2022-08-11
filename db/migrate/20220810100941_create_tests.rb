82fd290ba42b62bfd87dec0bff926f5010e86f78
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-11 17:18:35 +0300

    fix migrations

--- db/migrate/20220810100941_create_tests.rb
+++ db/migrate/20220810100941_create_tests.rb
@@ -2,8 +2,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default: 0
-      t.references :сategories, null: false, foreign_key: true
+      t.integer :level, default: 0, null: false
+      t.references :сategory, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: :users }
 
       t.timestamps
     end
