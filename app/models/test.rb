39f341e0d8733ee980d50a7e0637b0e5299a2034
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-11 20:29:59 +0300

    add methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+    def self.sort_category(category)
+        case category
+        when "Backend" then category_id = 2
+        when "Frontend" then category_id = 1
+        end  
+        pp Test.where("category_id = ?, category_id").order(title: :desc)
+    end    
 end
