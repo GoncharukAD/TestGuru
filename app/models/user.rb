47f39319977ff26ea77b24a4313f84e45d6e4e81
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-12 21:36:20 +0300

    rm pp in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     def used_tests(level)
-      pp  Test.joins("INNER JOIN results").where("results.user_id = ? AND tests.level = ?", self.id, level)
+      Test.joins("INNER JOIN results").where("results.user_id = ? AND tests.level = ?", self.id, level)
     end
 end
