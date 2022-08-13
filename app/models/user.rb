88dff29cb40d11cb5f0e1fef6a6ade4b2d4c5392
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-13 14:11:03 +0300

    fix uxer model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     def used_tests(level)
-      Test.joins("INNER JOIN results").where("results.user_id = ? AND tests.level = ?", self.id, level)
+      Test.joins("INNER JOIN results ON results.test_id = tests.id").where("results.user_id = ? AND tests.level = ?", self.id, level)
     end
 end
