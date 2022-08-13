01f34f89e06a0f317d7cbae4e799a95b33431cc1
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-13 16:07:51 +0300

    indents

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-    def self.tests_category(category) 
+  def self.tests_category(category) 
     Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order(title: :desc).pluck(:title)
-    end    
+  end    
 end
