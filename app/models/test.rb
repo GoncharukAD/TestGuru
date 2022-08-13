984349362a1ba38ba6c4914c9fc27110703094e3
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-13 16:06:46 +0300

    refactoring test and user methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-    def self.sort_category(category) 
-        Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order(title: :desc).pluck("tests.title")
+    def self.tests_category(category) 
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order(title: :desc).pluck(:title)
     end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    def used_tests(level)
-      Test.joins("INNER JOIN results ON results.test_id = tests.id").where("results.user_id = ? AND tests.level = ?", self.id, level)
-    end
+  def used_tests(level)
+    Test.joins("INNER JOIN results ON results.test_id = tests.id").where(results: {user_id: self.id}, tests: {level: level})
+  end
 end
