f7150774408a76a9c747a71424dfe63e5e22abfb
Anton Goncharuk <goncharuk.official@gmail.com>
2022-08-20 19:54:12 +0300

    fix association and method in user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
-  has_one :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
   
+  belongs_to :test
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :result
-  belongs_to :user, class_name: "Author", foreign_key: "author_id"
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
   
   has_many :questions, dependent: :destroy
 
   def self.tests_category(category) 
-    Test.joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+    joins(:category)
+      .where(categories: { title: category_title })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
