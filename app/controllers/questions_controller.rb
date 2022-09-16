1f12dfa83eb3b796afe41837565e868f332b1513
Anton Goncharuk <goncharuk.official@gmail.com>
2022-09-16 21:37:15 +0300

    Add question controllers methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,48 @@
 class QuestionsController < ApplicationController
+  
+  before_action :find_question
+
+  rescue from ActiveRecord::RecordNotFound, with: rescue_with_question_not_found
 
   def index 
-    
-    render plain: 'Просмотр всех вопросов'
+    render plain: 'Просмотр всех вопросов' #Возвращает только это,без списка вопросов
 
-    render json { tests: questions } #как обратиться к тесту,чтобы посмотреть у него вопросы?
-    
-    end  
+    @test = Test.find(params[:test_id])
+    render :json { questions: @test.questions }  
   end
 
   def show 
-    render plain: 'Просмотр конекретного вопроса теста'
+    render plain: 'Просмотр конкретного вопроса теста'
+    @test = Test.find(params[:test_id])
+    @test.questions.find(params[:id])
   end  
   
   def destroy 
     render plain: 'Удаление вопроса'
+    @question = Question.find(params[:id])
+    @question.destroy
   end
   
 
-  def create 
+  def new
     render plain: 'Создание вопроса'
+    @question = Question.create(
+      title: params[:question][:title]
+      test_id: params[:question][:test_id]
+    )
+  end
+  
+  def create
+  end 
+  
+  private
+
+  def find_question
+    @test = Question.find(params[:test_id])
+    @test.questions.find(params[:id])
   end  
-    
+
+  def rescue_with_question_not_found
+    render plain: 'Вопрос не найден' 
+  end  
 end
