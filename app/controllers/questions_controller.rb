9225c89cd7f68506cebc7715d2378a8d8ee0d8c9
Anton Goncharuk <goncharuk.official@gmail.com>
2022-10-12 13:01:49 +0300

    fix question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,44 +7,39 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = Test.find(params[:test_id]).questions
-    respond_to do |format|
-      format.html { render plain: 'Просмотр всех вопросов' } # index.html.erb
-      format.xml  { render xml: @questions }
-      format.json { render json: @questions }
-    end
+    render json: @questions.inspect 
   end
 
   def show
-    @question = Test.find(params[:test_id]).questions.find(params[:id])
+    @question = Question.find(params[:id])
     render plain: @question.inspect
   end
 
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-    render plain: 'Вопрос удален'
   end
 
-  def new; end
+  def new 
+  end
 
   def create
     @question = Question.create(question_params)
-    if question.save!
-      render plain: @question.inspect
+    if @question.save
+      @question.show
     else
-      new
+      render :new
     end
   end
 
   private
 
   def question_params
-    params.require(:question).permit(:title, :test_id)
+    params.require(:question).permit(:title)
   end
 
   def find_question
-    @test = Question.find(params[:test_id])
-    @test.questions.find(params[:id])
+    Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
