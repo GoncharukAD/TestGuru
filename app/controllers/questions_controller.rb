597c2c51c7e24226971b686f2c69ef2622df49c1
Anton Goncharuk <64324025+GoncharukAD@users.noreply.github.com>
2022-10-21 17:52:22 +0300

    Rest (#10)
    * feat : install yarn&webpacker and add Tests&Questions
    
    * Add question controllers methods
    
    * fix question_controller
    
    * webpacker update
    
    * fix question
    
    * fix webpacker
    
    * fix questions_controller.rb
    
    * Change README and questions_controler.rb
    
    * change render action in create method
    
    * fix
    
    * fix def destroy

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[destroy show]
+  before_action :find_test, only: %i[index create new]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render json: @test.questions.inspect 
+  end
+
+  def show
+    render plain: @question.inspect
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to @question.test
+  end
+
+  def new 
+    @question = @test.questions.new
+  end
+
+  def create
+    @new_question = @test.questions.new(question_params)
+    if @new_question.save
+      redirect_to @new_question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Вопрос не найден'
+  end
+end
