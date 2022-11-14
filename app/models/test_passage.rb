84a036fe16155c36d0c7a1992e96d0a03418a6ee
Anton Goncharuk <goncharuk.official@gmail.com>
2022-11-14 14:46:21 +0300

    fix

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -43,7 +43,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    answer_ids && correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
+    answer_ids.to_a && correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
