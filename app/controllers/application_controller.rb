9b7a7517750f83e19e2563401951a0391afa82b9
Anton Goncharuk <goncharuk.official@gmail.com>
2022-11-16 16:15:06 +0300

    devise fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      tests_path
+    end
+  end
+
 end
