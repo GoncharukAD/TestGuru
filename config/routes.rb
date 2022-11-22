5abccbd7393b7edc634e1c61fac72109c32cc6af
Anton Goncharuk <goncharuk.official@gmail.com>
2022-11-22 12:45:52 +0300

    fix migrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, controllers: { sessions: "users/sessions" }
 
-  get "welcome/index"
-  root to: "welcome#index"
+  # get "welcome/index"
+  # root to: "welcome#index"
 
-  # root 'tests#index'
+  root 'tests#index'
 
   resources :tests, only: :index  do
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2022_11_14_153017) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "quetions", column: "current_question_id"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
