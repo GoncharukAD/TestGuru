7a408de0965dc971283dd17f40c8d0359fbfcdad
Anton Goncharuk <goncharuk.official@gmail.com>
2022-11-25 17:25:53 +0300

    fix gists from lesson

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,6 +1,6 @@
 class GitHubClient
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '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'#лучше хранить через ENV['GITHUB_TOKEN']
+  ACCESS_TOKEN = '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' #лучше хранить через ENV['GITHUB_TOKEN']
 
   def initialize
     @http_client = setup_http_client
@@ -8,7 +8,7 @@ class GitHubClient
 
   def create_gist(params)
     @http_client.post('gists') do |request|
-      request.headers['Authentication'] = "token #{ACCESS_TOKEN}"
+      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
     end
