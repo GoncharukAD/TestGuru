df67eaa58638c787ab83cd86dd96daa48700a886
Anton Goncharuk <64324025+GoncharukAD@users.noreply.github.com>
2022-12-01 09:44:31 +0300

    Gists (#17)
    * gists from lesson
    
    * add gist model,octokit gemfile and fix git_hub_client
    
    * fix gists from lesson
    
    * add ENV
    
    * add oktokit client,gist view and controllers
    
    * add gists create,locales,view
    
    * old fix
    
    * Add gist controller for user,Struct in gist service
    
    * fix gist create
    
    * remove create gist from test_passage to gist_controller
    
    * fix gists
    
    * fix routes
    
    * fix gist_controller
    
    * gists fix
    
    * refactoring gists_controller
    
    * fix gists_controller
    
    * update gists_controller

---
+++ app/controllers/gists_controller.rb
@@ -0,0 +1,16 @@
+class GistsController < ApplicationController
+
+  def create
+    test_passage = TestPassage.find( params.require(:test_passage_id))
+    result = GistQuestionService.new(test_passage.current_question).call
+
+    if result.success?
+      current_user.gists.create!(
+        question_id: test_passage.current_question.id,
+        gist_url: result.url)
+
+        flash[:notice] = view_context.link_to(t('.success', url: result.url), target: '_blank')
+      end
+    redirect_to test_passage
+  end
+end
