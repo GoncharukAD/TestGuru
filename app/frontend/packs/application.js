90acd34ce4ce588a2295d84ee9c1458c0f35a09d
Anton Goncharuk <goncharuk.official@gmail.com>
2022-12-13 14:15:27 +0300

    fix js and scss

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "app\frontend\utilities"
 import '../js/bootstrap_js_files.js'
+
+require("packs/utilities/sorting")
+
 
 Rails.start()
 Turbolinks.start()
