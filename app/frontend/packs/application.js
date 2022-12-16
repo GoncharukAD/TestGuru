c472d6d9f5485c19617d20f565926f4cbf2d9710
Anton Goncharuk <goncharuk.official@gmail.com>
2022-12-16 18:23:15 +0300

    add sort from lesson for test js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,8 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("./sorting")
-require("./password_check")
+// require("./sorting")
+// require("./password_check")
+require("./sort_lesson")
 
 
 import Rails from "@rails/ujs"
