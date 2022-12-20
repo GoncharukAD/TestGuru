51ffb5b3e5c5d6c2e0e4b42d8165cf97a00d2228
Anton Goncharuk <goncharuk.official@gmail.com>
2022-12-20 22:39:19 +0300

    Add JS from lesson

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../bootstrap_js_files.js'
 
+require("utilities/sorting")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
